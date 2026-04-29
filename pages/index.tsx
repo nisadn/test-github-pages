a2ef77365f0e3b60df35a681808c7716dc5ef6e1
nisadn <annisa.nugrahani@traveloka.com>
2026-04-30 01:57:27 +0700

    dummy change

--- pages/index.tsx
+++ pages/index.tsx
@@ -94,7 +94,7 @@ export default function Home() {
               width={16}
               height={16}
             />
-            Examples
+            Examples - 30 Apr 2026
           </a>
           <a
             href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
