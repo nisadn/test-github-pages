5e7a9932a16d8292eba7752a35df4514b1ee374c
nisadn <annisa.nugrahani@traveloka.com>
2026-04-30 02:48:16 +0700

    minor

--- pages/index.tsx
+++ pages/index.tsx
@@ -94,7 +94,7 @@ export default function Home() {
               width={16}
               height={16}
             />
-            Examples - 30 Apr 2026
+            Examples - 30 Apr 2026 - 1
           </a>
           <a
             href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
